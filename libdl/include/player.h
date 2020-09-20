741d2891a84e1369b5040ef595780d5060dca1c0
Daniel Gerendasy <danova5648@gmail.com>
2020-09-20 04:33:15 -0700

    Documented the player struct in libdl player.h

--- libdl/include/player.h
+++ libdl/include/player.h
@@ -26,6 +26,24 @@
 #define PLAYER_MAX_HEALTH                   (50)
 
 /*
+ * NAME :		PlayerCameraType
+ * 
+ * DESCRIPTION :
+ * 			Defines the three player camera types.
+ *          These are used in the player struct under 'CameraType'
+ * 
+ * NOTES :
+ * 
+ * AUTHOR :			Daniel "Dnawrkshp" Gerendasy
+ */
+enum PlayerCameraType
+{
+    CAMERA_TYPE_THIRD_PERSON,
+    CAMERA_TYPE_LOCK_STRAFE,
+    CAMERA_TYPE_FIRST_PERSON
+};
+
+/*
  * NAME :		CameraAngle
  * 
  * DESCRIPTION :
@@ -59,154 +77,158 @@ typedef struct CameraAngle
  */
 typedef struct Player
 {
-    char UNK0[0xA0];
+    char UNK0[0xA0];                                                // 0x00
 
-    float PlayerX;
-    float PlayerY;
-    float PlayerZ;
+    float PlayerX;                                                  // 0xA0
+    float PlayerY;                                                  // 0xA4
+    float PlayerZ;                                                  // 0xA8
     
-    char UNK1[0x0C];
+    char UNK1[0x0C];                                                // 0xAC
 
-    float PlayerYaw;
+    float PlayerYaw;                                                // 0xB8
 
-    char UNK2[0x240];
+    char UNK2[0x240];                                               // 0xBC
 
-    int Airwalk;
+    int Airwalk;                                                    // 0x2FC
 
-    char UNK3[0x70];
+    char UNK3[0x70];                                                // 0x300
 
     // This resets whenever you jump/cboot/land
     // Appears to count how long since you've entered a state
     // Setting it to 0 when cbooting gives infinite cbooting
-    int TicksSinceStateChanged;
+    int TicksSinceStateChanged;                                     // 0x370
     
     // More state related ticks
-    int StateTicks[5];
+    int StateTicks[5];                                              // 0x374
 
-    char UNK4[0x28];
+    char UNK4[0x28];                                                // 0x388
 
 
-    int RespawnTimer;
+    int RespawnTimer;                                               // 0x3B0
 
-    char UNK30[0x38];
+    char UNK30[0x38];                                               // 0x3B4
 
-    float WeaponCooldownTimer;
+    float WeaponCooldownTimer;                                      // 0x3EC
 
-    char UNK5[0x18];
+    char UNK5[0x18];                                                // 0x3F0
 
-    short ShieldTimer;
-    short QuadTimer;
-    int UNK6;
-    short UNK7;
-    short FireTimer;
-    short AcidTimer;
-    short FreezeTimer;
-    short UNK8;
-    short JuggyElectricityTimer;
-    short UNK9;
-    short BlowupTimer;
+    short ShieldTimer;                                              // 0x408
+    short QuadTimer;                                                // 0x40A
+    int UNK6;                                                       // 0x40C
+    short UNK7;                                                     // 0x410
+    short FireTimer;                                                // 0x412
+    short AcidTimer;                                                // 0x414
+    short FreezeTimer;                                              // 0x416
+    short UNK8;                                                     // 0x418
+    short JuggyElectricityTimer;                                    // 0x41A
+    short UNK9;                                                     // 0x41C
+    short BlowupTimer;                                              // 0x41E
 
-    char UNK10[0x3DC];
+    char UNK10[0x3DC];                                              // 0x420
 
     // First instance of weapon held id
     // This one is zero for any item that isn't a gun
     // So wrench and swingshot are 0
-    int WeaponHeldGun;
+    int WeaponHeldGun;                                              // 0x7FC
 
-    char UNK11[0x1150];
+    char UNK11[0x1150];                                             // 0x800
 
-    Vector4 CameraPos;
-    char UNK99[0x50];
+    Vector4 CameraPos;                                              // 0x1950
+    char UNK99[0x50];                                               // 0x1960
 
-    CameraAngle CameraYaw;
-    CameraAngle CameraPitch;
+    CameraAngle CameraYaw;                                          // 0x19B0
+    CameraAngle CameraPitch;                                        // 0x19D0
     
-    char UNK12[0x20];
+    char UNK12[0x20];                                               // 0x19F0
 
-    char UNK13[0xC];
+    char UNK13[0xC];                                                // 0x1A10
 
     // This is a pointer to the moby that the
     // player is looking/aiming at
-    Moby * LookAtMoby;
+    Moby * LookAtMoby;                                              // 0x1A1C
 
-    char UNK14[0x04];
+    char UNK14[0x04];                                               // 0x1A20
 
-    float CameraPitchMin;
-    float CameraPitchMax;
+    float CameraPitchMin;                                           // 0x1A24
+    float CameraPitchMax;                                           // 0x1A28
 
-    Moby * SkinMoby;
+    Moby * SkinMoby;                                                // 0x1A2C
 
-    float CameraDistance;
+    float CameraDistance;                                           // 0x1A30
 
-    char UNK15[0x68];
+    char UNK15[0x68];                                               // 0x1A34
 
-    int LocalPlayerIndex;
+    int LocalPlayerIndex;                                           // 0x1A9C
 
-    char UNK16[0x8];
+    char UNK16[0x8];                                                // 0x1AA0
 
-    int PlayerId;
+    int PlayerId;                                                   // 0x1AA8
 
-    char UNK17[0x84C];
+    char UNK33[0x34];                                               // 0x1AAC
+
+    int CameraType;                                                 // 0x1AE0
+
+    char UNK17[0x814];                                              // 0x1AE4
 
     // When 1, the player is trying to shoot
-    int IsShooting;
+    int IsShooting;                                                 // 0x22F8
 
-    char UNK29[0x18];
+    char UNK29[0x18];                                               // 0x22FC
 
     // When this value is set to 3,
     // The weapon held is changed to the weapon id
     // set in WeaponHeldId
-    int WeaponSwitchState;
-    int WeaponSwitchLastWeaponHeldId;
+    int WeaponSwitchState;                                          // 0x2314
+    int WeaponSwitchLastWeaponHeldId;                               // 0x2318
 
-    char UNK28[0x194];
+    char UNK28[0x194];                                              // 0x231C
 
-    int WeaponHeldId0;
+    int WeaponHeldId0;                                              // 0x24B0
 
-    char UNK18[0x118];
+    char UNK18[0x118];                                              // 0x24B4
 
-    int PlayerState;
+    int PlayerState;                                                // 0x25CC
 
-    char UNK19[0x94];
+    char UNK19[0x94];                                               // 0x25D0
 
-    int IsDead;
+    int NerfPlayer;                                                 // 0x2664
 
-    char UNK31[0xC];
+    char UNK31[0xC];                                                // 0x2668
 
-    int ChangeWeaponHeldId;
+    int ChangeWeaponHeldId;                                         // 0x2674
 
-    char UNK20[0x14];
+    char UNK20[0x14];                                               // 0x2678
 
-    int WeaponHeldId;
+    int WeaponHeldId;                                               // 0x268C
 
-    char UNK27[0x54];
+    char UNK27[0x54];                                               // 0x2690
 
-    u32 WeaponHeldDataPointer;
+    u32 WeaponHeldDataPointer;                                      // 0x26E4
 
-    char UNK21[0x738];
+    char UNK21[0x738];                                              // 0x26E8
     
-    float Health;
+    float Health;                                                   // 0x2E20
     
-    char UNK22[0xCC];
+    char UNK22[0xCC];                                               // 0x2EEC
 
-    // Points to the player moby (0x2EF0)
-    Moby * PlayerMoby;
+    // Points to the player moby
+    Moby * PlayerMoby;                                              // 0x2EF0
 
-    char UNK23[0x0C];
+    char UNK23[0x0C];                                               // 0x2EFC
 
-    PadButtonStatus * Paddata;
+    PadButtonStatus * Paddata;                                      // 0x2F00
 
-    char UNK24[0x10];
+    char UNK24[0x10];                                               // 0x2F04
 
-    int Team;
+    int Team;                                                       // 0x2F14
 
-    char UNK25[0x98];
+    char UNK25[0x98];                                               // 0x2F18
 
-    float DamageMultiplier;
+    float DamageMultiplier;                                         // 0x2FB0
 
-    char UNK26[0x0C];
+    char UNK26[0x0C];                                               // 0x2FB4
 
-    float Speed;
+    float Speed;                                                    // 0x2FC0
 
 } Player;
 
